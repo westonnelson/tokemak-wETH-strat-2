19d3c20c76b7183d47509c391ee6437dcb44d22c
Weston Nelson <westontnelson@outlook.com>
2022-03-21 02:30:33 -0500

    Update

--- interfaces/tokemak/ILiquidityETHPool.sol
+++ interfaces/tokemak/ILiquidityETHPool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "./IWETH.sol";
 import "./IManager.sol";
--- interfaces/tokemak/IWETH.sol
+++ interfaces/tokemak/IWETH.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
